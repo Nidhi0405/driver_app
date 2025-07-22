23ac0cfe620c7a6f194ae0227e3860b7bdb0ef90
Nidhi <nidhimaniyar003@gmail.com>
2025-07-22 09:27:55 +0530

    Push directly to OTP Screen without verify Phone Number in mobileAuthServices.dart due to Firebase Billing.

--- lib/view/authScreens/otpScreen.dart
+++ lib/view/authScreens/otpScreen.dart
@@ -4,6 +4,7 @@ import 'package:driver_app/controller/provider/authProvider/mobileAuthProvider.d
 import 'package:driver_app/utils/colors.dart';
 import 'package:driver_app/utils/textStyles.dart';
 import 'package:driver_app/view/authScreens/mobileLoginScreen.dart';
+import 'package:driver_app/view/bottomNavigationBar/bottomNavigationBar.dart';
 import 'package:driver_app/view/home/homeScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -46,132 +47,140 @@ class _OTPScreenState extends State<OTPScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: Stack(
-        children: [
-          Positioned(
-            left: 10.w,
-            bottom: 3.h,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  PageTransition(
-                    child: MobileLoginScreen(),
-                    type: PageTransitionType.leftToRight,
-                  ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                shape: CircleBorder(),
-                padding: EdgeInsets.all(1.h),
-                backgroundColor: greyShade3,
-                elevation: 2,
-              ),
-              child: FaIcon(
-                FontAwesomeIcons.arrowLeft,
-                size: 3.h,
-                color: black,
-              ),
-            ),
-          ),
-
-          Positioned(
-            right: 5.w,
-            bottom: 3.h,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  PageTransition(
-                    child: HomeScreen(),
-                    type: PageTransitionType.rightToLeft,
-                  ),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                shape: StadiumBorder(),
-                padding: EdgeInsets.symmetric(horizontal: 3.w, vertical: 1.h),
-                backgroundColor: greyShade3,
-                elevation: 2,
-              ),
-              child: Row(
-                children: [
-                  Text("Next", style: AppTextStyles.body14),
-                  SizedBox(width: 2.w),
-                  FaIcon(FontAwesomeIcons.arrowRight, size: 3.h, color: black),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-
-      body: ListView(
-        padding: EdgeInsets.symmetric(horizontal: 3.w, vertical: 2.h),
-        children: [
-          SizedBox(height: 2.h),
-          Text(
-            "Enter the 4-digit code sent to you at ${context.read<MobileAuthProvider>().mobileNumber}",
-            style: AppTextStyles.body16,
-          ),
-
-          SizedBox(height: 3.h),
-
-          PinCodeTextField(
-            appContext: context,
-            length: 6,
-            obscureText: false,
-            animationType: AnimationType.fade,
-            pinTheme: PinTheme(
-              shape: PinCodeFieldShape.box,
-              borderRadius: BorderRadius.circular(5),
-              fieldHeight: 50,
-              fieldWidth: 50,
-              activeFillColor: Colors.white,
-              inactiveColor: greyShade3,
-              inactiveFillColor: greyShade3,
-              selectedFillColor: white,
-              selectedColor: black,
-              activeColor: black,
-            ),
-            animationDuration: const Duration(milliseconds: 300),
-            backgroundColor: transparent,
-            enableActiveFill: true,
-            errorAnimationController: errorController,
-            controller: otpController,
-            onCompleted: (v) {
-              print("Completed");
-            },
-            onChanged: (value) {},
-            beforeTextPaste: (text) {
-              print("Allowing to paste $text");
-              //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
-              //but you can show anything you want here, like your pop up saying wrong paste format or etc
-              return true;
-            },
-          ),
-
-          SizedBox(height: 4.h),
-
-          Row(
-            children: [
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 1.h),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50.sp),
-                  color: greyShade3,
+    return SafeArea(
+      child: Scaffold(
+        floatingActionButton: Stack(
+          children: [
+            Positioned(
+              left: 10.w,
+              bottom: 3.h,
+              child: ElevatedButton(
+                onPressed: () {
+                  //Firebase is not working due to BILLING_UNAVAILABLE error so checkAuthentication may not work.
+                  Navigator.push(
+                    context,
+                    PageTransition(
+                      child: MobileLoginScreen(),
+                      type: PageTransitionType.leftToRight,
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: CircleBorder(),
+                  padding: EdgeInsets.all(1.h),
+                  backgroundColor: greyShade3,
+                  elevation: 2,
                 ),
-                child: Text(
-                  "I haven't received a code (0.$resendOTPCounter)",
-                  style: AppTextStyles.small12.copyWith(
-                    color: resendOTPCounter > 0 ? black38 : black,
-                  ),
+                child: FaIcon(
+                  FontAwesomeIcons.arrowLeft,
+                  size: 3.h,
+                  color: black,
                 ),
               ),
-            ],
-          ),
-        ],
+            ),
+
+            Positioned(
+              right: 5.w,
+              bottom: 3.h,
+              child: ElevatedButton(
+                onPressed: () {
+                  //Firebase is not working due to BILLING_UNAVAILABLE error so checkAuthentication may not work.
+                  Navigator.push(
+                    context,
+                    PageTransition(
+                      child: BottomNavigationBarUberEats(),
+                      type: PageTransitionType.rightToLeft,
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: StadiumBorder(),
+                  padding: EdgeInsets.symmetric(horizontal: 3.w, vertical: 1.h),
+                  backgroundColor: greyShade3,
+                  elevation: 2,
+                ),
+                child: Row(
+                  children: [
+                    Text("Next", style: AppTextStyles.body14),
+                    SizedBox(width: 2.w),
+                    FaIcon(
+                      FontAwesomeIcons.arrowRight,
+                      size: 3.h,
+                      color: black,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+
+        body: ListView(
+          padding: EdgeInsets.symmetric(horizontal: 3.w, vertical: 2.h),
+          children: [
+            SizedBox(height: 2.h),
+            Text(
+              "Enter the 4-digit code sent to you at ${context.read<MobileAuthProvider>().mobileNumber}",
+              style: AppTextStyles.body16,
+            ),
+
+            SizedBox(height: 3.h),
+
+            PinCodeTextField(
+              appContext: context,
+              length: 6,
+              obscureText: false,
+              animationType: AnimationType.fade,
+              pinTheme: PinTheme(
+                shape: PinCodeFieldShape.box,
+                borderRadius: BorderRadius.circular(5),
+                fieldHeight: 50,
+                fieldWidth: 50,
+                activeFillColor: Colors.white,
+                inactiveColor: greyShade3,
+                inactiveFillColor: greyShade3,
+                selectedFillColor: white,
+                selectedColor: black,
+                activeColor: black,
+              ),
+              animationDuration: const Duration(milliseconds: 300),
+              backgroundColor: transparent,
+              enableActiveFill: true,
+              errorAnimationController: errorController,
+              controller: otpController,
+              onCompleted: (v) {
+                print("Completed");
+              },
+              onChanged: (value) {},
+              beforeTextPaste: (text) {
+                print("Allowing to paste $text");
+                //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
+                //but you can show anything you want here, like your pop up saying wrong paste format or etc
+                return true;
+              },
+            ),
+
+            SizedBox(height: 4.h),
+
+            Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 4.w, vertical: 1.h),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50.sp),
+                    color: greyShade3,
+                  ),
+                  child: Text(
+                    "I haven't received a code (0.$resendOTPCounter)",
+                    style: AppTextStyles.small12.copyWith(
+                      color: resendOTPCounter > 0 ? black38 : black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
